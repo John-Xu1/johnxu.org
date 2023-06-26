2d20676987d2f015a47eac6a20e02e212cb67308
John-Xu1 <XuJohn400@gmail.com>
2023-06-26 19:59:27 -0400

    Header responsive

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -6,6 +6,7 @@ import "react-icons/ai";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 import { RiSendPlaneFill } from "react-icons/ri";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const NavItem = ({ href, label, target }) => {
   return (
@@ -22,59 +23,76 @@ const NavItem = ({ href, label, target }) => {
 };
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   console.log(usePathname());
   return (
     <>
       {usePathname() !== "/resume" && usePathname() !== "/resume.pdf" && (
         <>
-          <div className="flex py-3 justify-between w-100vw">
-            <div className="flex ml-32">
+          <div className="flex py-3 justify-center sm:justify-between w-100vw">
+            <div className="flex ml-0 sm:ml-[5vw]">
               <NavItem href="/" label="Home" />
               <NavItem href="/about" label="About" />
               <NavItem href="/resume.pdf" label="Resume" target="_blank" />
             </div>
-            <div className="flex gap-8 mr-32">
-              <a
-                href="mailto:john.xu@duke.edu"
-                className="flex justify-center"
-                target="_blank"
-              >
-                <RiSendPlaneFill
-                  className="h-auto hover:fill-emerald-400 hover:rotate-12 hover:scale-110 hover:cursor-pointer duration-300"
-                  size={32}
-                />
-              </a>
-              <a
-                href="https://linkedin.com/in/john-j-xu"
-                className="flex justify-center"
-                target="_blank"
-              >
-                <AiFillLinkedin
-                  className="h-auto hover:fill-blue-600 hover:-rotate-6 hover:scale-110 hover:cursor-pointer duration-300"
-                  size={32}
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/john_xu22/"
-                className="flex justify-center"
-                target="_blank"
-              >
-                <AiFillInstagram
-                  className="h-auto hover:fill-rose-500 hover:rotate-45 hover:scale-110 hover:cursor-pointer duration-300"
-                  size={32}
-                />
-              </a>
-              <a
-                href="https://github.com/John-Xu1"
-                className="flex justify-center"
-                target="_blank"
-              >
-                <AiFillGithub
-                  className="h-auto hover:fill-violet-700 hover:-rotate-12 hover:scale-110 hover:cursor-pointer duration-300"
-                  size={32}
-                />
-              </a>
-            </div>
+            {!isMobile && (
+              <div className="flex gap-8 mr-[5vw]">
+                <a
+                  href="mailto:john.xu@duke.edu"
+                  className="flex justify-center"
+                  target="_blank"
+                >
+                  <RiSendPlaneFill
+                    className="h-auto hover:fill-emerald-400 hover:rotate-12 hover:scale-110 hover:cursor-pointer duration-300"
+                    size={32}
+                  />
+                </a>
+                <a
+                  href="https://linkedin.com/in/john-j-xu"
+                  className="flex justify-center"
+                  target="_blank"
+                >
+                  <AiFillLinkedin
+                    className="h-auto hover:fill-blue-600 hover:-rotate-6 hover:scale-110 hover:cursor-pointer duration-300"
+                    size={32}
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/john_xu22/"
+                  className="flex justify-center"
+                  target="_blank"
+                >
+                  <AiFillInstagram
+                    className="h-auto hover:fill-rose-500 hover:rotate-45 hover:scale-110 hover:cursor-pointer duration-300"
+                    size={32}
+                  />
+                </a>
+                <a
+                  href="https://github.com/John-Xu1"
+                  className="flex justify-center"
+                  target="_blank"
+                >
+                  <AiFillGithub
+                    className="h-auto hover:fill-violet-700 hover:-rotate-12 hover:scale-110 hover:cursor-pointer duration-300"
+                    size={32}
+                  />
+                </a>
+              </div>
+            )}
           </div>
           <hr />
         </>
